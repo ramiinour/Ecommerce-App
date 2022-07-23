3220f9c9fe3085a3e9d464eeba21d63cbb2451e8
raminour <60442943+ramiinour@users.noreply.github.com>
2022-07-23 21:17:50 +0300

    adding info to Footer and Navbar

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { AiFillInstagram, AiOutlineTwitter, AiFillHeart } from "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
